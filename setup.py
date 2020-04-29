944fdc7a539d856f409f7ecc8dc9dff64289ce07
Jonas <40004508+spameier@users.noreply.github.com>
2020-04-29 17:02:05 +0200

    :tada: initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='placemark_linker',
+    version='0.0.1',
+    description='Link placemark to other areas represented with KML files',
+    author='spameier',
+    author_email='spameier@protonmail.ch',
+    license='MIT',
+    install_requires=requirements,
+    packages=['placemark_linker'],
+    entry_points={
+        'console_scripts': ['placemark_linker=placemark_linker.placemark_linker:main']
+    }
+)
